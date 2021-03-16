0ca5679bfdc60e016e2329cc790575024c204486
LucasDL423 <lucas.lalande@univ-lyon1.fr>
2021-03-16 19:28:54 +0100

    First commit :punch: Initialization folders

---
+++ script/00_setup.R
@@ -0,0 +1,13 @@
+# first script 
+# Lucas Lalande
+# 16/03/2021
+
+# create folders
+
+folder_test_script <- file.path("script")
+dir.create(file.path(folder_test_script))
+
+folder_test_results <- file.path("results")
+dir.create(file.path(folder_test_results))
+
+rm()
